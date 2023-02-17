be6fcf11a606cdec1c0a76763bc3dc76c44fb96e
Kelvin <ngotho.kelvin@yahoo.com>
2023-02-17 09:51:10 +0300

    add components

---
+++ src/components/rockets/Rockets.js
@@ -0,0 +1,3 @@
+// import React, { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { fetchRockets, bookRockets } from '../../redux/rockets/rocketSlice';
