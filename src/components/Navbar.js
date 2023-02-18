b5ff7b6870ef236931b4dc82d936a1bf2ff1eb68
Kelvin <ngotho.kelvin@yahoo.com>
2023-02-18 10:18:56 +0300

    resolve image path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../images/planet.png';
+import logo from './images/planet.png';
 
 const Navbar = () => {
   const activePage = ({ isActive }) => ({
